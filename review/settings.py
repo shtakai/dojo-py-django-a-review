ad7ffc8ead2764e8678f525fc87c56af2009a9a7
Alyson Takai <shtakai@gmail.com>
2016-05-22 14:11:21 -0700

    finished assignment review

--- review/settings.py
+++ review/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login_reg',
+    'message_board',
+    'game_page',
 ]
 
 MIDDLEWARE_CLASSES = [
